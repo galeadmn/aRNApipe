4cd4927fb3bd9bb908960fb96a29f51d4f82f775
root <galeadmn@uw.edu>
2019-01-23 16:07:35 -0800

    removed spurious file

--- lib/my.py
+++
@@ -1,8 +0,0 @@
-import optparse
-import time
-import os
-import config as config
-import refbuild as refbuild
-
-res = refbuild.annotate_gtf("/share/genomes_processed/Macaca_mulatta/genesets.gtf")
-print res
