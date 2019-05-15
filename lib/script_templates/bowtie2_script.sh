3875e080b2eeb0536e4943d404ece5c412a252c4
root <galeadmn@uw.edu>
2019-05-15 14:56:47 -0700

    Preserved raw read data

--- lib/script_templates/bowtie2_script.sh
+++ lib/script_templates/bowtie2_script.sh
@@ -1,6 +1,8 @@
-/usr/local/bin/bowtie2 -p 6 BOWTIE2_OPTIONS -x BOWTIE2_INDEX  --un-conc-gz OUTPUT_DIR/OUTFILE -1 READ1 -2 READ2 > /dev/null 2> OUTPUT_DIR/ERR_OUTFILE && (echo "SAMPLE_NAME" >> OUTPUT_DIR/samples_ok.txt) || (echo "$HOSTNAME SAMPLE_NAME" >> OUTPUT_DIR/samples_ko.txt)
+# Run bowtie2 to remove the RNA and/or globin
+/usr/local/bin/bowtie2 -p 6 BOWTIE2_OPTIONS -x BOWTIE2_INDEX  --un-conc-gz OUTPUT_DIR/OUTFILE -1 READ1 -2 READ2 > /dev/null 2> OUTPUT_DIR/ERR_OUTFILE && (echo "SAMPLE_NAME" >> OUTPUT_DIR/samples_ok.txt; rm -f READ1 READ2) || (echo "$HOSTNAME SAMPLE_NAME" >> OUTPUT_DIR/samples_ko.txt)
 
-grep "aligned concordantly" OUTPUT_DIR/ERR_OUTFILE > OUTPUT_DIR/RNA_REMOVAL_REPORT
+grep "reads; of these:"  OUTPUT_DIR/ERR_OUTFILE > OUTPUT_DIR/RNA_REMOVAL_REPORT
+grep "aligned concordantly" OUTPUT_DIR/ERR_OUTFILE >> OUTPUT_DIR/RNA_REMOVAL_REPORT
 grep "overall alignment rate" OUTPUT_DIR/ERR_OUTFILE >> OUTPUT_DIR/RNA_REMOVAL_REPORT
 rm -f OUTPUT_DIR/ERR_OUTFILE
 
